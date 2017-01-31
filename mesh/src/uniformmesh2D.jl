2dfbd8d5f690a9cea3cf361807d75ecec5dac386
Derek Handwerk <drhandwerk@Dereks-MacBook-Pro.local>
2017-01-30 17:45:02 -0700

    added mesh triangle generation

--- mesh/src/uniformmesh2D.jl
+++ mesh/src/uniformmesh2D.jl
@@ -24,11 +24,14 @@ function generateVertices(mesh::Uniformmesh2D)
   for i = 1:mesh.m+1
     for j = 1:mesh.n+1
       mesh.verticies[j + (i-1)*(mesh.n+1)] = Vertex(width*(i-1),height*(j-1))
-      println(i,j)
     end
   end
 end
 # Generate array of triangles numbered from bottom left. Increases up then right.
 function generateTriangles(mesh::Uniformmesh2D)
-
+  for i = 1:mesh.m
+    for j = 1:2*mesh.n
+        mesh.triangles[j + (i-1)*mesh.n*2] = Triangle(mesh.verticies[1],mesh.verticies[4],mesh.verticies[5])
+    end
+  end
 end

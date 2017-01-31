67c626c0c4369f13625154d3895f6a7631d9e67d
Derek Handwerk <drhandwerk@Dereks-MacBook-Pro.local>
2017-01-30 17:15:23 -0700

    TODO for triangle

--- mesh/src/triangle.jl
+++ mesh/src/triangle.jl
@@ -2,6 +2,6 @@ type Triangle
   p1::Vertex
   p2::Vertex
   p3::Vertex
-
+  #TODO order the points counter clockwise. Maybe have a sort function for Vertex
   Triangle(p1,p2,p3) = new(p1,p2,p3)
 end

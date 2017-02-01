a45d43a28c8696fb2c3c87594af4b28354c8eb63
Derek Handwerk <drhandwerk@gmail.com>
2017-02-01 14:55:02 -0700

    Added comments to Vertex

--- mesh/src/vertex.jl
+++ mesh/src/vertex.jl
@@ -1,7 +1,15 @@
+"""
+  Vertex(x, y)
+
+Creates a two-dimensional vertex point ('x','y'). Inherits AbstractArray for all the built in Vector
+functions. Entries are floats. 
+
+"""
 type Vertex <: AbstractArray{Float64, 1}
   x::Float64
   y::Float64
 end
+# These functions are needed for the inheritence.
 Base.size(V::Vertex) = (2,)
 function Base.getindex(V::Vertex, i::Int)
   if i == 1

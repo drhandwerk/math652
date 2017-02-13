9aca091bf07e21dcfc4f8b73851438f4058fa4c4
Derek Handwerk <handwerk@math.colostate.edu>
2017-02-13 13:53:05 -0700

    poissonsolve works

---
+++ UniformMesh/src/PoissonSolver.jl
@@ -0,0 +1,107 @@
+
+include("./UniformMesh.jl")
+using UniformMesh
+using Cubature # for quadrature
+
+export poissonsolve
+
+function poissonsolve(n::Int64)
+  # get mesh
+  mesh = UniformRectMesh(n,n)
+
+  # get global stiffness matrix
+  G = gsm(mesh)
+
+  # set RHS from quadrature over elements
+  b = rhs(mesh)
+
+  # set BC
+  setdirichlet!(mesh,G,b)
+
+  # computer coeffs
+  c = G\b
+
+  # return as matrix
+  return reshape(c,n+1,n+1)
+end
+
+"""
+  elementrhs()
+Do quadrature over an element for RHS.
+"""
+function elementrhs(length::Int64, p1::Array{Float64,1}, p2::Array{Float64,1})
+  area = (p2[1] - p1[1]) * (p2[2] - p1[2])
+  (b1, err1) = hcubature(x -> 2*pi^2*sin(pi*x[1])*sin(pi*x[2]) * ((p2[1] - x[1])*(p2[2] - x[2]))/area, p1, p2)
+  (b2, err2) = hcubature(x -> 2*pi^2*sin(pi*x[1])*sin(pi*x[2]) * ((x[1] - p1[1])*(p2[2] - x[2]))/area, p1, p2)
+  (b3, err3) = hcubature(x -> 2*pi^2*sin(pi*x[1])*sin(pi*x[2]) * ((x[1] - p1[1])*(x[2] - p1[2]))/area, p1, p2)
+  (b4, err4) = hcubature(x -> 2*pi^2*sin(pi*x[1])*sin(pi*x[2]) * ((p2[1] - x[1])*(x[2] - p1[2]))/area, p1, p2)
+  return [b1;b2;b3;b4]
+end
+
+"""
+  rhs(mesh)
+Assemlbe the vector b (RHS) by summing over elements.
+"""
+function rhs(mesh::UniformRectMesh)
+  b = zeros(Float64, size(mesh.vertices,1), 1) # preallocate
+  for i = 1:size(mesh.rectangles,1)
+    elemb = elementrhs(size(mesh.vertices,1),
+                       mesh.vertices[mesh.rectangles[i,1],:],
+                       mesh.vertices[mesh.rectangles[i,3],:])
+    b[mesh.rectangles[i,:]] += elemb
+  end
+  return b
+end
+"""
+  esmrect(p1, p2)
+Send in lower left corner 'p1' and upper right corner 'p2' of a rectangle. Return the
+element stiffness matrix.
+"""
+function esmrect(p1::Array{Float64,1}, p2::Array{Float64,1})
+  dx = p2[1] - p1[1]
+  dy = p2[2] - p1[2]
+  E = zeros(Float64, 4, 4)
+  A11 = 1./3. * (dx/dy + dy/dx)
+  A12 = 1./6. * dx/dy - 1./3. * dy/dx
+  A13 = -1./6. * (dx/dy + dy/dx)
+  A14 = -1./3. * dx/dy + 1./6. * dy/dx
+
+  E = [A11 A12 A13 A14;
+       A12 A11 A14 A13;
+       A13 A14 A11 A12;
+       A14 A13 A12 A11]
+
+end
+
+"""
+ build gsm from esms for rectangle
+"""
+function gsm(mesh::UniformRectMesh)
+  G = zeros(Float64, size(mesh.vertices,1), size(mesh.vertices,1)) # preallocate
+  for i = 1:size(mesh.rectangles,1)
+    E = esmrect(mesh.vertices[mesh.rectangles[i,1],:],
+                mesh.vertices[mesh.rectangles[i,3],:])
+    G[mesh.rectangles[i,:], mesh.rectangles[i,:]] += E
+  end
+  return G
+end
+
+
+"""
+
+"""
+function setdirichlet!(mesh::UniformRectMesh, G::Array{Float64, 2}, b::Array{Float64,2})
+  #b = zeros(size(mesh.vertices,1), 1) # preallocate
+  # get boundary vertices
+  V = reshape(1:size(mesh.vertices,1), mesh.m+1, mesh.n+1)
+  interiorvertices = V[2:end-1, 2:end-1][:] # vector
+  exteriorvertices = setdiff(1:size(mesh.vertices,1), interiorvertices)
+
+  ind = trues(size(G)) # indices to zero out
+  ind[interiorvertices,:] = false # keep interiorvertices rows
+  for i in exteriorvertices # keep what's on the diagonal
+    ind[i,i] = false
+  end
+  G[ind] = 0.0
+  b[exteriorvertices] = 0.0
+end
--- UniformMesh/src/UniformMesh.jl
+++ UniformMesh/src/UniformMesh.jl
@@ -1,17 +1,212 @@
+__precompile__()
 module UniformMesh
 
-"""
-  UniformMesh(n,m)
+using PyPlot
 
-Abstract type for a uniform mesh. Subtypes could include triangle or rectangle.
+export UniformTriangleMesh
+export UniformRectMesh
+export drawmesh
+
+abstract UniformPolyMesh
+
+type UniformRectMesh <: UniformPolyMesh
+  m::Int64
+  n::Int64
+  vertices::Array{Float64,2}
+  rectangles::Array{Int64,2}
+  edges::Array{Int64,2}
+
+  function UniformRectMesh(m::Int64,n::Int64)
+    mesh = new(m,
+               n,
+               Array{Float64, 2}((m+1)*(n+1), 2),
+               Array{Int64, 2}(m*n, 4),
+               Array{Int64, 2}(2*m*n + n + m, 2))
+    generateVertices!(mesh)
+    generateRectangles!(mesh)
+    generateEdges!(mesh)
+    return mesh
+  end
+end
+
 """
-abstract UniformMesh
+  UniformTriangleMesh(m,n)
+
+Creates a uniform triangle mesh of the unit square with 'm' partitions in the
+x direction and 'n' partitions in the y direction.
+
+TODO: should triangles and edges be points (currently) or the indices of the
+vertices? e.g.
+mesh.triangles[count, 1:2] = mesh.vertices[base, :]
+mesh.triangles[count, 3:4] = mesh.vertices[base + (mesh.n + 1), :]
+mesh.triangles[count, 5:6] = mesh.vertices[base + (mesh.n + 2), :]
+
+or
+
+mesh.triangles[count, 1] = base
+mesh.triangles[count, 2] = base + (mesh.n + 1)
+mesh.triangles[count, 3] = base + (mesh.n + 2)
+Don't forget to change constructor if this is implemented
+"""
+type UniformTriangleMesh <: UniformPolyMesh
   m::Int64 # Number of partitions in x-dir
   n::Int64 # Number of partitions in y-dir
-  num_vertices::Int64
   vertices::Array{Float64, 2}
-  triangles::Array{Float64, 2}
-  edges::Array{Float64, 2}
+  triangles::Array{Int64, 2}
+  edges::Array{Int64, 2}
+
+  function UniformTriangleMesh(m::Int64,n::Int64)
+    mesh = new(m,
+               n,
+               Array{Float64, 2}((m+1)*(n+1), 2),
+               Array{Int64, 2}(m*n*2, 3),
+               Array{Int64, 2}(3*m*n + n + m, 2))
+    generateVertices!(mesh)
+    generateTriangles!(mesh)
+    generateEdges!(mesh)
+    return mesh
+  end
+end
+
+"""
+  generateVertices!(mesh)
+
+Creates all of the vertices for the mesh and changes in place. Numbering starts
+at the bottom left, and goes up each column before going to the bottom of the next
+column.
+"""
+function generateVertices!(mesh::UniformPolyMesh)
+  width::Float64 = 1.0/mesh.m
+  height::Float64 = 1.0/mesh.n
+  for i = 1:mesh.m+1
+    for j = 1:mesh.n+1
+      mesh.vertices[j + (i-1)*(mesh.n+1), 1] = width*(i-1)  # x component
+      mesh.vertices[j + (i-1)*(mesh.n+1), 2] = height*(j-1) # y component
+    end
+  end
+end
+
+"""
+  generateRectangles!(UniformRectMesh)
+
+Creates all of the rectangles(indices of vertices) for the mesh overwriting the Constructor created
+empty array in place. Numbering starts at bottom left, proceeds up, then right.
+"""
+function generateRectangles!(mesh::UniformRectMesh)
+  count::Int32 = 1
+  for i = 1:mesh.m
+    for j = 1:mesh.n
+      base::Int64 = j + (mesh.n + 1)*(i-1)
+      mesh.rectangles[count, 1] = base
+      mesh.rectangles[count, 2] = base + (mesh.n + 1)
+      mesh.rectangles[count, 3] = base + (mesh.n + 2)
+      mesh.rectangles[count, 4] = base + 1
+      count += 1
+    end
+  end
+end
+
+"""
+  generateTriangles!(mesh)
+
+Creates all of the triangles(indices of vertices) for the mesh overwriting the Constructor created
+empty array in place. Numbering starts at bottom left, proceeds up, then right.
+"""
+function generateTriangles!(mesh::UniformTriangleMesh)
+  count::Int32 = 1
+  for i = 1:mesh.m
+    for j = 1:mesh.n
+      base::Int64 = j + (mesh.n + 1)*(i-1)
+      # two triangles to maintain vertex ordering
+      mesh.triangles[count, 1] = base
+      mesh.triangles[count, 2] = base + (mesh.n + 1)
+      mesh.triangles[count, 3] = base + (mesh.n + 2)
+      count += 1
+      mesh.triangles[count, 1] = base
+      mesh.triangles[count, 2] = base + (mesh.n + 2)
+      mesh.triangles[count, 3] = base + 1
+      count += 1
+    end
+  end
+end
+
+"""
+  generateEdges!(UniformRectMesh)
+
+Creates all of the edges for the mesh, overwriting the default Constructor
+generated Array in the process. First count vertical edges, then horizontal edges.
+"""
+function generateEdges!(mesh::UniformRectMesh)
+  count::Int32 = 1
+  # Vertical Edges
+  for j = 1:mesh.m+1
+    for i = 1:mesh.n
+      mesh.edges[count, 1] = i + (mesh.n + 1)*(j - 1)
+      mesh.edges[count, 2] = (i + 1) + (mesh.n + 1)*(j - 1)
+      count += 1
+    end
+  end
+  # Horizontal Edges
+  for j = 1:mesh.m
+    for i = 1:mesh.n+1
+      mesh.edges[count, 1] = i + (mesh.n + 1)*(j - 1)
+      mesh.edges[count, 2] = (i + mesh.n + 1) + (mesh.n + 1)*(j - 1)
+      count += 1
+    end
+  end
+end
+
+"""
+  generateEdges!(UniformTriangleMesh)
+
+Creates all of the edges for the mesh, overwriting the default Constructor
+generated Array in the process. First count vertical edges, then horizontal edges,
+then diagonal edges.
+"""
+function generateEdges!(mesh::UniformTriangleMesh)
+  count::Int32 = 1
+  # Vertical Edges
+  for j = 1:mesh.m+1
+    for i = 1:mesh.n
+      mesh.edges[count, 1] = i + (mesh.n + 1)*(j - 1)
+      mesh.edges[count, 2] = (i + 1) + (mesh.n + 1)*(j - 1)
+      count += 1
+    end
+  end
+  # Horizontal Edges
+  for j = 1:mesh.m
+    for i = 1:mesh.n+1
+      mesh.edges[count, 1] = i + (mesh.n + 1)*(j - 1)
+      mesh.edges[count, 2] = (i + mesh.n + 1) + (mesh.n + 1)*(j - 1)
+      count += 1
+    end
+  end
+  # Diagonal Edges
+  for j = 1:mesh.m
+    for i = 1:mesh.n
+      mesh.edges[count, 1] = i + (mesh.n + 1)*(j - 1)
+      mesh.edges[count, 2] = (i + mesh.n + 2) + (mesh.n + 1)*(j - 1)
+      count += 1
+    end
+  end
+end
+
+"""
+  drawmesh(mesh)
+
+Plot the vertices and edges for the mesh.
+"""
+function drawmesh(mesh::UniformPolyMesh)
+  if mesh.n > 32 || mesh.m > 32
+    throw(ArgumentError("Drawing a fine mesh(>32,>32) is a bad idea."))
+  end
+  # Edges
+  for i = 1:size(mesh.edges,1)
+  plot(mesh.vertices[mesh.edges[i,:],1], mesh.vertices[mesh.edges[i,:],2],linestyle="-",color="green")
+  end
+  # Vertices
+  scatter(mesh.vertices[:,1], mesh.vertices[:,2], marker="o", s = 30, color="blue")
+  axis("square")
 end
 
 end # End module
